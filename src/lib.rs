14acef1ce1adac98f0b441a1a1cadaad9361215a
TVYanaE <yana.for.spam@gmail.com>
2025-11-25 22:14:16 +0100

    RenderPipeline has been implemented

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,9 @@ use winit::{
 use wgpu::{
     Device, Queue, Surface, TextureFormat, Instance, InstanceDescriptor,
     RequestAdapterOptions, DeviceDescriptor, CommandEncoderDescriptor, RenderPassDescriptor,
-    RenderPassColorAttachment,TextureViewDescriptor, Operations, SurfaceConfiguration,
+    RenderPassColorAttachment,TextureViewDescriptor, Operations, SurfaceConfiguration, RenderPipelineDescriptor,
+    PipelineLayoutDescriptor, RenderPipeline, ShaderModuleDescriptor, 
+    ShaderSource, VertexState, PipelineCompilationOptions, FragmentState, PrimitiveState, PrimitiveTopology, MultisampleState,
 };
 
 struct State {
@@ -19,6 +21,7 @@ struct State {
     size: winit::dpi::PhysicalSize<u32>,
     surface: Surface<'static>,
     surface_format: TextureFormat,
+    render_pipeline: RenderPipeline,
 }
 
 impl State {
@@ -39,6 +42,82 @@ impl State {
         let cap = surface.get_capabilities(&adapter);
         let surface_format = cap.formats[0];
 
+        let surface_config = SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            // Request compatibility with the sRGB-format texture view we‘re going to create later.
+            view_formats: vec![surface_format.add_srgb_suffix()],
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            width: size.width,
+            height: size.height,
+            desired_maximum_frame_latency: 2,
+            present_mode: wgpu::PresentMode::AutoVsync,
+        };
+        surface.configure(&device, &surface_config);
+
+        let shader_module_descriptor = ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: ShaderSource::Wgsl(include_str!("shader.wgsl").into()), 
+        };
+
+        let shader_module = device.create_shader_module(shader_module_descriptor);
+
+        let pipe_line_layout_descriptor = PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        };
+
+        let pipe_line_layout = device.create_pipeline_layout(&pipe_line_layout_descriptor);
+
+        let vertex_state = VertexState {
+            module: &shader_module,
+            entry_point: Some("vs_main"),
+            buffers: &[],
+            compilation_options: PipelineCompilationOptions::default(), 
+        };
+
+        let fragment_state = FragmentState {
+            module: &shader_module,
+            entry_point: Some("fs_main"),
+            targets: &[Some(wgpu::ColorTargetState {
+                format: surface_config.format,
+                blend: Some(wgpu::BlendState::REPLACE),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+            compilation_options: PipelineCompilationOptions::default(),
+        };
+
+        let primitive_state = PrimitiveState {
+            topology: PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: Some(wgpu::Face::Back),
+            polygon_mode: wgpu::PolygonMode::Fill,
+            unclipped_depth: false,
+            conservative: false,
+        };
+
+        let multisample_state = MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false
+        };
+
+        let render_pipeline_description = RenderPipelineDescriptor {
+            label: Some("Render pipeline"),
+            layout: Some(&pipe_line_layout),
+            vertex: vertex_state,
+            fragment: Some(fragment_state),
+            primitive: primitive_state,
+            depth_stencil: None,
+            multisample: multisample_state,
+            multiview: None,
+            cache: None,
+        };
+
+        let render_pipeline = device.create_render_pipeline(&render_pipeline_description);
+
         let state = State {
             window,
             device,
@@ -46,6 +125,7 @@ impl State {
             size,
             surface,
             surface_format,
+            render_pipeline,
         };
 
         // Configure surface for the first time
@@ -59,18 +139,7 @@ impl State {
     }
 
     fn configure_surface(&self) {
-        let surface_config = SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: self.surface_format,
-            // Request compatibility with the sRGB-format texture view we‘re going to create later.
-            view_formats: vec![self.surface_format.add_srgb_suffix()],
-            alpha_mode: wgpu::CompositeAlphaMode::Auto,
-            width: self.size.width,
-            height: self.size.height,
-            desired_maximum_frame_latency: 2,
-            present_mode: wgpu::PresentMode::AutoVsync,
-        };
-        self.surface.configure(&self.device, &surface_config);
+        
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
@@ -116,8 +185,11 @@ impl State {
             color_attachments: &[Some(collor_attachments)],
         };
 
-        let render_pass = command_encoder.begin_render_pass(&render_pass_description);
-      
+        let mut render_pass = command_encoder.begin_render_pass(&render_pass_description);
+       
+        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.draw(0..3, 0..1);
+
         drop(render_pass);
 
         let command_buffer = command_encoder.finish();
